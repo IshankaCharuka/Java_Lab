7cfd1d14c6e4796d87a94845e3df19a996f86345
IshankaCharuka <135942692+IshankaCharuka@users.noreply.github.com>
2023-07-31 21:33:21 +0530

    Add files via upload

---
+++ Lab14/Student5/src/main/java/com/mycompany/student5/Student5.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.student5;
+
+public class Student5 {  
+    final int marks = 100;
+
+    final void display() 
+    {
+        System.out.println("Marks: " + marks);
+    }
+
+
+    }
